988957f2aa865c41ce85c16f05879d0a8ca330b9
bamboo <nmz89825@gmail.com>
2024-09-21 01:19:42 +0800

    fix: fix 26 sources path to make it display as normal.

--- src/course/chapter3-advanced-techniques/26-code-structuring-for-bigger-projects/experience/sources.ts
+++ src/course/chapter3-advanced-techniques/26-code-structuring-for-bigger-projects/experience/sources.ts
@@ -11,28 +11,28 @@ const sources: Source[] = [
 		name: "environmentMapTexture",
 		type: "cubeTexture",
 		path: [
-			"textures/environmentMap/px.jpg",
-			"textures/environmentMap/nx.jpg",
-			"textures/environmentMap/py.jpg",
-			"textures/environmentMap/ny.jpg",
-			"textures/environmentMap/pz.jpg",
-			"textures/environmentMap/nz.jpg",
+			"/textures/environmentMaps/5/px.jpg",
+			"/textures/environmentMaps/5/nx.jpg",
+			"/textures/environmentMaps/5/py.jpg",
+			"/textures/environmentMaps/5/ny.jpg",
+			"/textures/environmentMaps/5/pz.jpg",
+			"/textures/environmentMaps/5/nz.jpg",
 		],
 	},
 	{
 		name: "grassColorTexture",
 		type: "texture",
-		path: "textures/dirt/color.jpg",
+		path: "/textures/dirt/color.jpg",
 	},
 	{
 		name: "grassNormalTexture",
 		type: "texture",
-		path: "textures/dirt/normal.jpg",
+		path: "/textures/dirt/normal.jpg",
 	},
 	{
 		name: "foxModel",
 		type: "gltfModel",
-		path: "models/Fox/glTF/Fox.gltf",
+		path: "/models/Fox/glTF/Fox.gltf",
 	},
 ];
 
