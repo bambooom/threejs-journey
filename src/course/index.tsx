3753832c6048772cf62bb205a0ecc66d5cbadbc3
bamboo <nmz89825@gmail.com>
2024-09-20 16:36:23 +0800

    change: update the home index for all lesson's page.

--- src/course/index.tsx
+++ src/course/index.tsx
@@ -46,6 +46,59 @@ const Page: FC = () => {
           <Link to="/chapter1-basics/13-go-live">13 Go Live</Link>
         </li>
       </ul>
+      <div>Chapter 2: Classic Techniques</div>
+      <ul>
+        <li>
+          <Link to="/chapter2-classic-techniques/14-lights">14 Lights</Link>
+        </li>
+        <li>
+          <Link to="/chapter2-classic-techniques/15-shadows">15 Shadows</Link>
+        </li>
+        <li>
+          <Link to="/chapter2-classic-techniques/16-haunted-house">
+            16 Haunted House
+          </Link>
+        </li>
+        <li>
+          <Link to="/chapter2-classic-techniques/17-particles">
+            17 Particles
+          </Link>
+        </li>
+        <li>
+          <Link to="/chapter2-classic-techniques/18-galaxy-generator">
+            18 Galaxy Generator
+          </Link>
+        </li>
+        <li>
+          <Link to="/chapter2-classic-techniques/19-scroll-based-animation">
+            19 Scroll Based Animation
+          </Link>
+        </li>
+      </ul>
+      <div>Chapter 3: Advanced Techniques</div>
+      <ul>
+        <li>
+          <Link to="/chapter3-advanced-techniques/20-physics">20 Physics</Link>
+        </li>
+        <li>
+          <Link to="/chapter3-advanced-techniques/21-imported-models">21 Imported Models</Link>
+        </li>
+        <li>
+          <Link to="/chapter3-advanced-techniques/22-raycaster-and-mouse-events">21 Raycaster and Mouse Events</Link>
+        </li>
+        <li>
+          <Link to="/chapter3-advanced-techniques/23-custom-models-with-blender">23 Custom Models with Blender</Link>
+        </li>
+        <li>
+          <Link to="/chapter3-advanced-techniques/24-environment-map">24 Environment Map</Link>
+        </li>
+        <li>
+          <Link to="/chapter3-advanced-techniques/25-realistic-render">25 Realistic Render</Link>
+        </li>
+        <li>
+          <Link to="/chapter3-advanced-techniques/26-code-structuring-for-bigger-projects">26 Code Structuring for Bigger Projects</Link>
+        </li>
+      </ul>
     </>
   );
 };
